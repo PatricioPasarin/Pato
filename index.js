04686af3a53fbd1c86809752ff6a349b0cfb3e20
Patricio Pasarin <patricio.pasarin@gmail.com>
2023-08-22 16:08:26 -0300

    proyecto js parte 2

--- index.js
+++ index.js
@@ -8,15 +8,20 @@ const  base_de_datos = [
         password: "1234"
     },
     {
-        user: "",
-        password: ""
+        user: "profe",
+        password: "1234"
     }
     
 ]
 
+const existeUser = JSON.parse(localStorage.getItem("user"))
 const user = document.querySelector("#floatingInput");
 const password = document.querySelector("#floatingPassword");
 const botonEnviar = document.querySelector("#enviar");
+
+if (existeUser !== undefined){
+    // logeo.innerHTML= '<h2 class="msjLogeo"> Welcome </h2> <a href="/index.html" class="btn btn-propio">INICIO</a>'
+}
 
 const validacion = {
     user: '',
@@ -34,9 +39,12 @@ password.addEventListener("input",(event)=>{
 botonEnviar.addEventListener("click",()=>{
     const validacionExitosa = base_de_datos.find(user=> user.user == validacion.user && user.password == validacion.password)
     if (validacionExitosa === undefined){
-        logeo.innerHTML= '<h2 class="msjLogeo"> user not found </h2> <a href="/pages/Login.html" class="btn btn-propio">retry</a>'
+        logeo.innerHTML= '<h2 class="msjLogeo"> user not found </h2> <a href="/pages/Login.html" class="btn btn-primary">retry</a>'
     }else{
         const logeo = document.querySelector("#logeo");
-        logeo.innerHTML= '<h2 class="msjLogeo"> Welcome </h2> <a href="/index.html" class="btn btn-propio">INICIO</a>'
+        logeo.innerHTML= '<h2 id="ingresa"> Welcome </h2> <a href="/index.html" class="btn btn-primary">Start</a>'
+        localStorage.setItem("user",JSON.stringify(validacionExitosa))
     }
 })
+
+localStorage.removeItem("base de datos")
